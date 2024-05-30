1f28dbe2a0f4f04db05c61944608205dc31fb5a7
Mamush-Meshesha <mamgithub@gmail.com>
2024-05-30 23:35:36 +0300

    re usable component component

---
+++ components/visualEffect/water-wave.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { FC, ReactNode } from "react";
+import WaterWave from "react-water-wave";
+
+interface WaterWaveWrapperProps {
+  imageUrl: string;
+  dropRadius: string;
+  perturbance: string;
+  resolution: string;
+  children: () => ReactNode;
+}
+const WaterWaveWrapper: FC<WaterWaveWrapperProps> = ({
+  children,
+  imageUrl,
+  resolution,
+  dropRadius,
+  perturbance,
+}) => {
+  return (
+    <WaterWave
+      imageUrl={imageUrl}
+      dropRadius={dropRadius}
+      perturbance={perturbance}
+      resolution={resolution}
+    >
+      {children}
+    </WaterWave>
+  );
+};
+
+export default WaterWaveWrapper;
